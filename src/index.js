f1c016746f8bba35a2cd3fe9d892c5ef87e3231a
oscary <oscarygs26@hotmail.com>
2019-08-28 16:37:33 -0400

    50% del Tab_hero

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import  Tab_hero from "./Components/Tab_hero"
+import TabHero from "./Components/TabHero/TabHero"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const container = document.getElementById('app');
 
 
-ReactDOM.render(<Tab_hero />, container);
+ReactDOM.render(<TabHero Title=" Reserva un viaje.Hospeda viajeros.Todo en Airbnb." />, container);
