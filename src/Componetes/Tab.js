6c0e9e03293fe10daa43b257dc4f72d2d3d49a6c
oscary garcia <elniodelrap2@hotmail.com>
2019-08-27 20:57:57 -0400

    poniendolo clear

---
+++ src/Componetes/Tab.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+
+class Tab extends React.Components{
+    render(){
+        return(
+        <div className="Container">
+            <h1>Reserva un viaje. Hospeda viajeros. Todo en Airbnb.</h1>
+            <div className="box__left">
+            <p>Encuentra cosas que hacer y lugares para hospedarte</p>
+            <div className="button__left">
+                <h3>Explorar</h3>
+            </div>
+            </div>
+            <div className="box">
+
+            <div className="box__right">
+            <p>Encuentra cosas que hacer y lugares para hospedarte</p>
+            <div className="button__right">
+                Anfitrion
+            </div>
+            </div>
+            </div>
+        </div>
+
+        );
+    }
+}
+
+
+export default  Tab;
