dc44825c5b7e418fd4e65952c3b7dc94400295a0
oscary garcia <elniodelrap2@hotmail.com>
2019-08-28 23:17:53 -0400

    add nuevo componente

---
+++ src/Components/Reservation/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Reservation extends Component {
+    render() {
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <h2>Reserva alojamientos y experiencias únicos</h2>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Reservation;
