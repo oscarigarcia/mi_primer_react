c93766add8f0cf03f8d98ef461f837f5937d96f5
oscary garcia <elniodelrap2@hotmail.com>
2019-08-28 23:13:16 -0400

    se completo el tab!

--- src/Components/TabHero/TabHero.js
+++ src/Components/TabHero/TabHero.js
@@ -1,33 +1,37 @@
 import React, { Component } from "react";
 import "./styles.scss";
+import Reservation from './../Reservation/index';
 
 class TabHero extends Component {
   render() {
     const { Title } = this.props;
     return (
-      <div className="container__tab">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-10">
-              <h1>{Title}</h1>
-              <div className="row">
-                <div className="col-lg-6">
-                  <div className="box__left">
-                    <p>Encuentra cosas que hacer y lugares para hospedarte</p>
-                    <div className="button__left">Explorar</div>
+      <React.Fragment>
+        <div className="container__tab">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-10">
+                <h1>{Title}</h1>
+                <div className="row">
+                  <div className="col-lg-6">
+                    <div className="box">
+                      <p>Encuentra cosas que hacer y lugares para hospedarte</p>
+                      <div className="button__left">Explorar</div>
+                    </div>
                   </div>
-                </div>
-                <div className="col-lg-6">
-                  <div className="box__right">
-                    <p>Gana dinero con el espacio que no usas</p>
-                    <div className="button__right">Anfitrión</div>
+                  <div className="col-lg-6">
+                    <div className="box">
+                      <p>Gana dinero con el espacio que no usas</p>
+                      <div className="button__right">Anfitrión</div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+        <div className="col-12"> <Reservation /></div>
+      </React.Fragment>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const container = document.getElementById('app');
 
 
-ReactDOM.render(<TabHero Title=" Reserva un viaje.Hospeda viajeros.Todo en Airbnb." />, container);
+ReactDOM.render(<TabHero Title=" Reserva un viaje. Hospeda viajeros.Todo en Airbnb."/>, container);
