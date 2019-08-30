3de7eaabfa4506187e275c3e906c8ee6f9c543c2
oscary <oscarygs26@hotmail.com>
2019-08-30 16:01:45 -0400

    terminado el componente de criticas

--- src/Components/Benefits/index.js
+++ src/Components/Benefits/index.js
@@ -8,34 +8,37 @@ class Benefits extends Component {
         <div className="container">
           <div className="row">
             <div className="col-lg-12">
-              <h4 className="cards">Viajando con Airbnb</h4>
-              <div className="row ">
-                <div className="col-lg-4">
-                  <span>
-                    Servicio al cliente las 24 horas, los 7 días de la semana
-                  </span>
-                  <p>
-                    De día o de noche, estamos aquí para ayudarte. Habla con
-                    nuestro equipo de servicio al cliente estés donde estés y a
-                    cualquier hora.
-                  </p>
-                </div>
-                <div className="col-lg-4">
-                  <span>Estándares de hospitalidad global</span>
-                  <p>
-                    Los huéspedes evalúan a sus anfitriones después de cada
-                    estancia. Para formar parte de Airbnb, todos los anfitriones
-                    deben tener una calificación mínima y cumplir nuestros
-                    estándares de hospitalidad.
-                  </p>
-                </div>
-                <div className="col-lg-4">
-                  <span>Anfitriones de 5 estrellas</span>
-                  <p>
-                    Nuestros anfitriones son pura hospitalidad. Disfruta de unas
-                    sábanas recién puestas y atiende sus consejos sobre los
-                    mejores sitios para ir a comer.
-                  </p>
+              <h4>Viajando con Airbnb</h4>
+
+              <div className="cards">
+                <div className="row ">
+                  <div className="col-lg-4">
+                    <span>
+                      Servicio al cliente las 24 horas, los 7 días de la semana
+                    </span>
+                    <p>
+                      De día o de noche, estamos aquí para ayudarte. Habla con
+                      nuestro equipo de servicio al cliente estés donde estés y
+                      a cualquier hora.
+                    </p>
+                  </div>
+                  <div className="col-lg-4">
+                    <span>Estándares de hospitalidad global</span>
+                    <p>
+                      Los huéspedes evalúan a sus anfitriones después de cada
+                      estancia. Para formar parte de Airbnb, todos los
+                      anfitriones deben tener una calificación mínima y cumplir
+                      nuestros estándares de hospitalidad.
+                    </p>
+                  </div>
+                  <div className="col-lg-4">
+                    <span>Anfitriones de 5 estrellas</span>
+                    <p>
+                      Nuestros anfitriones son pura hospitalidad. Disfruta de
+                      unas sábanas recién puestas y atiende sus consejos sobre
+                      los mejores sitios para ir a comer.
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
--- src/Components/Critics/index.js
+++ src/Components/Critics/index.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import "./styles.scss";
-import habitacion from "./habitacion.jpg";
-
+import habitacion from "../../Images/habitacion.jpg";
+import piscina from "../../Images/piscina.jpg";
+import sala from "../../Images/sala.jpg";
+import user from "../../Images/user.jpg";
+import julia from "../../Images/julia.jpg";
+import eugenia from "../../Images/eugenia.jpg";
 class Critics extends Component {
   render() {
     return (
@@ -12,46 +16,70 @@ class Critics extends Component {
               <h4>
                 Qué dicen los huéspedes sobre los alojamientos en Estados Unidos
               </h4>
+              <div className="sub-title">
+                Los alojamientos en Estados Unidos fueron calificados con
+                <strong> 4.8 estrellas de 5</strong> con más de
+                <strong> 31.500.000 evaluaciones</strong>
+              </div>
               <div className="row">
                 <div className="col-lg-4">
-                  <img src={habitacion} width="335" height="222" />
+                  <img src={piscina} width="335" height="222" alt="profile" />
                   <span>icon</span>
                   <p>
-                    Jim y Jacky son anfitriones muy agradables, y dispuestos a
-                    ayudar. El anuncio reflejaba bien la realidad del
-                    alojamiento.
+                    El vecindario es muy tranquilo y silencioso, muy cerca de
+                    restaurantes, el lugar de Donna tiene todo lo necesario para
+                    sentirte en casa, todo...
                   </p>
                   <div className="mini-profile">
-                    <img src={habitacion} />
-                    <span>Javier Perú</span>
-                  </div>
-                </div>
-                <div className="col-lg-4">
-                  <img src={habitacion} width="335" height="222" />
-                  <span>icon</span>
-                  <p>
-                    Douglas y Chris son una pareja encantadora. Muy amables y
-                    dispuestos a ayudar en todo momento. El estudio es muy
-                    bonito y lo tienen
-                  </p>
-                  <div>
-                    <div className="mini-profile">
-                      <img src={habitacion} />
-                      <span>Maria Eugenia Perú</span>
+                    <div className="mini-img">
+                      <img src={user} alt="profile" />
+                    </div>
+                    <div className="name">
+                      <div className="font-weight-bold">Marco</div>
+                      <span>México</span>
                     </div>
                   </div>
                 </div>
                 <div className="col-lg-4">
-                  <img src={habitacion} width="335" height="222" />
+                  <img src={sala} width="335" height="222" alt="profile" />
+                  <p>prueba</p>
+                  <p>
+                    Douglas y Chris son una pareja encantadora. Muy amables y
+                    dispuestos a ayudar en todo momento. El estudio es muy
+                    bonito y lo tienen...
+                  </p>
+                  <div>
+                    <div className="mini-profile">
+                      <div className="mini-img">
+                        <img src={julia} alt="profile" />
+                      </div>
+                      <div className="name">
+                        <div className="font-weight-bold">Maria Eugenia</div>
+                        <span>Perú</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-4">
+                  <img
+                    src={habitacion}
+                    width="335"
+                    height="222"
+                    alt="profile"
+                  />
                   <span>icon</span>
                   <p>
-                    Danielle me hizo sentir como en casa, fue muy amable todo el
-                    tiempo. Fue por mí al aeropuerto y me llevó diariamente al
-                    congreso
+                    La descripción es correcta, una habitación hermosa!! Y una
+                    anfitriona grandiosa...
                   </p>
                   <div className="mini-profile">
-                    <img src={habitacion} />
-                    <span>Anahi México</span>
+                    <div className="mini-img">
+                      <img src={eugenia} alt="profile" />
+                    </div>
+                    <div className="name">
+                      <div className="font-weight-bold">Julia</div>
+                      <span>Argentina</span>
+                    </div>
                   </div>
                 </div>
               </div>
